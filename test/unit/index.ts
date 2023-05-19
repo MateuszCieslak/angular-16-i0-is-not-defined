9300afd3e092edf0e5aba698cd0548fc7c32f202
MateuszCieslak <mateusz.cieslak2@gmail.com>
2023-05-19 11:52:09 +0200

    Create index.ts

---
+++ test/unit/index.ts
@@ -0,0 +1,8 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+platformBrowserDynamic();
+
+describe('foo', () => {
+    it('bar', () => {
+        expect(true).toBe(true);
+    });
+})
